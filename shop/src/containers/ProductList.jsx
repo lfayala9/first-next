c09c2d09f499e66a4fcc811769a738b9e274d9b3
Luis Felipe <lfayala9@gmail.com>
2023-04-24 19:28:57 -0400

    something

--- shop/src/containers/ProductList.jsx
+++ shop/src/containers/ProductList.jsx
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Product from '../components/Product';
-import useGetItems from '../hooks/useGetItems';
+// import useGetItems from '../hooks/useGetItems';
 import style from '@styles/ProductList.module.scss';
+import RotateLoader from 'react-spinners/RotateLoader';
 
 const API = 'https://fakestoreapi.com/products?offset=20&limit=12';
 
 const ProductList = () => {
-  const products = useGetItems(API);
+  const [products, setProduct] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const getProducts = async () => {
+      await fetch(API)
+        .then((res) => res.json())
+        .then((response) => {
+          setProduct(response);
+          setIsLoading(false);
+        });
+    };
+    getProducts();
+  }, []);
+  // const products = useGetItems(API);
   return (
     <section className={style['main-container']}>
       <div className={style.ProductList}>
         {products.map((product) => (
           <Product product={product} key={product.id} />
         ))}
+        {isLoading && <RotateLoader color={'#acd9b2'} loading={isLoading} size={150} />}
       </div>
     </section>
   );
--- shop/src/hooks/useGetItems.js
+++ shop/src/hooks/useGetItems.js
@@ -3,19 +3,21 @@ import { useState, useEffect } from 'react';
 
 const useGetItems = (API) => {
   const [products, setProduct] = useState([]);
-  // const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     const getProducts = async () => {
       await fetch(API)
         .then((res) => res.json())
         .then((response) => {
           setProduct(response);
-          // setIsLoading(false);
+          setIsLoading(false);
         });
     };
     getProducts();
   }, [API]);
+  if (isLoading) return <span>Cargando</span>;
 
   return products;
 };
